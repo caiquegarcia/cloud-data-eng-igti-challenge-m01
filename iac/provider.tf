1563e6de895ede5a37dde7afdf510cd0be81eac1
Caique Garcia <55662579+caiquegarcia@users.noreply.github.com>
2022-09-07 20:22:54 -0300

    Update provider.tf
    Delete aws credentials

--- iac/provider.tf
+++ iac/provider.tf
@@ -1,5 +1,3 @@
 provider "aws" {
   region     = "ca-central-1"
-  access_key = "AKIAVXAV74VIOQ4DUK3G"                     #"${{secrets.AWS_ACCESS_KEY}}"
-  secret_key = "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" #"${{secrets.AWS_SECRET_KEY}}"
 }
